44ffd3de08a8c6281fc83722c4c89db4969eb90b
Angela Kock <angela@jhb.dvt.co.za>
2019-05-14 08:11:27 +0200

    Removal of add event

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,13 @@ import { CalendarComponent } from './components/calendar/calendar.component';
 import { AboutOfficeComponent } from './components/about-office/about-office.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { NewEventComponent } from './components/new-event/new-event.component';
 
 const routes: Routes = [
 
   {
     path: 'profile',
     component: ProfileComponent
-  }, {
-    path: 'new-event',
-    component: NewEventComponent
-  }, {
+  },  {
     path: 'calendar',
     component: CalendarComponent
   }, {
